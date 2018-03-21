8925a3966dea2b5819297cb10aab4412f240ac0c
Benjamin Segall <bentekkie@gmail.com>
2018-03-21 18:02:52 -0400

    lets ecrypt attemp 2

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /**
  * Module dependencies.
- */
+*/
 
 var passport = require('passport');
 var Strategy = require('passport-http').BasicStrategy;
@@ -10,9 +10,59 @@ var express = require('express'),
     path = require('path'),
     favicon = require('serve-favicon');
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+
+function approveDomains(opts, certs, cb) {
+  // This is where you check your database and associated
+  // email addresses with domains and agreements and such
+
+
+  // The domains being approved for the first time are listed in opts.domains
+  // Certs being renewed are listed in certs.altnames
+  if (certs) {
+    opts.domains = certs.altnames;
+  }
+  else {
+    opts.email = 'john.doe@example.com';
+    opts.agreeTos = true;
+  }
+
+  // NOTE: you can also change other options such as `challengeType` and `challenge`
+  // opts.challengeType = 'http-01';
+  // opts.challenge = require('le-challenge-fs').create({});
+
+  cb(null, { options: opts, certs: certs });
+}
+
+
+var lex = require('greenlock-express').create({
+  // set to https://acme-v01.api.letsencrypt.org/directory in production
+  server: 'staging'
+
+// If you wish to replace the default plugins, you may do so here
+//
+, challenges: { 'http-01': require('le-challenge-fs').create({ webrootPath: '/tmp/acme-challenges' }) }
+, store: require('le-store-certbot').create({ webrootPath: '/tmp/acme-challenges' })
+
+// You probably wouldn't need to replace the default sni handler
+// See https://git.coolaj86.com/coolaj86/le-sni-auto if you think you do
+//, sni: require('le-sni-auto').create({})
+
+, approveDomains: [ 'bentekkiemainframe-env.aizyyr2mr9.us-west-2.elasticbeanstalk.com', 'www.bentekkiemainframe-env.aizyyr2mr9.us-west-2.elasticbeanstalk.com' ]
+});
+
+
+// handles acme-challenge and redirects to https
+var http = require('http').createServer(lex.middleware(require('redirect-https')())).listen(app.get('port'), function () {
+  console.log("Listening for ACME http-01 challenges on", this.address());
+});
+// handles your app
+var https = require('https').createServer(lex.httpsOptions, lex.middleware(app)).listen(app.get('port')+1, function () {
+  console.log("Listening for ACME tls-sni-01 challenges and serve app on", this.address());
+});
+
+var io = require('socket.io')(https);
 var a = require('./sockets')(io);
+
 // all environments
 
 /*
@@ -25,7 +75,7 @@ if(process.env.PORT){
     } else {
         next();
     }
-});  
+});
 }
 */
 
