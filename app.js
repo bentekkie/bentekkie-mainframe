8f94ae78999202c9f8b4c5e4887da93a8a9b253e
Benjamin Segall <bentekkie@gmail.com>
2018-03-22 11:12:09 -0400

    letsencrypt appempt 11

--- app.js
+++ app.js
@@ -11,46 +11,6 @@ var express = require('express'),
     favicon = require('serve-favicon');
 var app = express();
 
-function approveDomains(opts, certs, cb) {
-  // This is where you check your database and associated
-  // email addresses with domains and agreements and such
-
-
-  // The domains being approved for the first time are listed in opts.domains
-  // Certs being renewed are listed in certs.altnames
-  if (certs) {
-    opts.domains = certs.altnames;
-  }
-  else {
-    opts.email = 'john.doe@example.com';
-    opts.agreeTos = true;
-  }
-
-  // NOTE: you can also change other options such as `challengeType` and `challenge`
-  // opts.challengeType = 'http-01';
-  // opts.challenge = require('le-challenge-fs').create({});
-
-  cb(null, { options: opts, certs: certs });
-}
-
-
-var lex = require('greenlock-express').create({
-  // set to https://acme-v01.api.letsencrypt.org/directory in production
-  server: 'staging'
-
-// If you wish to replace the default plugins, you may do so here
-//
-, challenges: { 'http-01': require('le-challenge-fs').create({ webrootPath: '/tmp/acme-challenges' }) }
-
-,  store: require('le-store-certbot').create({ webrootPath: '/tmp/acme-challenges' }),
-// You probably wouldn't need to replace the default sni handler
-// See https://git.coolaj86.com/coolaj86/le-sni-auto if you think you do
-//, sni: require('le-sni-auto').create({})
-
-, approveDomains: [ 'bentekkiemainframe-env.aizyyr2mr9.us-west-2.elasticbeanstalk.com', 'www.bentekkiemainframe-env.aizyyr2mr9.us-west-2.elasticbeanstalk.com' ]
-});
-
-
 
 // all environments
 
@@ -94,14 +54,35 @@ app.get("/",function(req, res) {
     res.sendfile(path.join(__dirname ,'client' ,'build'+'/index.html'));
 })
 app.get('/file/:fname', file.download);
-// handles acme-challenge and redirects to https
-var http = require('http').createServer(lex.middleware(require('redirect-https')())).listen(parseInt(app.get('port')), function () {
-  console.log("Listening for ACME http-01 challenges on", this.address());
+
+
+
+const PROD = false;
+const lex = require('greenlock-express').create({
+  server: PROD ? 'https://acme-v01.api.letsencrypt.org/directory' : 'staging',
+
+  approveDomains: (opts, certs, cb) => {
+    if (certs) {
+      // change domain list here
+      opts.domains = ['bentekkiemainframe-env.aizyyr2mr9.us-west-2.elasticbeanstalk.com', 'bentekkie.com']
+    } else {
+      // change default email to accept agreement
+      opts.email = 'bentekkie@gmail.com';
+      opts.agreeTos = true;
+    }
+    cb(null, { options: opts, certs: certs });
+  }
 });
-// handles your app
-var https = require('https').createServer(lex.httpsOptions, lex.middleware(app)).listen(parseInt(app.get('port'))+1, function () {
-  console.log("Listening for ACME tls-sni-01 challenges and serve app on", this.address());
-});
+const middlewareWrapper = lex.middleware;
+
+var https = require('https').createServer(
+  lex.httpsOptions,
+  middlewareWrapper(app)
+);
+
 
 var io = require('socket.io')(https);
 var a = require('./sockets')(io);
+const redirectHttps = require('redirect-https');
+http.createServer(lex.middleware(redirectHttps())).listen(app.get('port'));
+https.listen(parseInt(app.get('port'))+1);
