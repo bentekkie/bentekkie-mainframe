206965616faaa462f82a5bb4567aa27f9a73e26e
Benjamin Segall <bentekkie@gmail.com>
2019-05-17 03:12:15 -0400

    added ssl

--- server/main.go
+++ server/main.go
@@ -1,21 +1,38 @@
 package server
 
 import (
+	"context"
+	"crypto/tls"
+	"fmt"
 	pb "github.com/bentekkie/bentekkie-mainframe/server/generated"
 	"github.com/bentekkie/bentekkie-mainframe/server/mainframe"
 	"github.com/bentekkie/bentekkie-mainframe/server/middleware"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/improbable-eng/grpc-web/go/grpcweb"
+	"golang.org/x/crypto/acme/autocert"
 	"google.golang.org/grpc"
 	"log"
 	"net/http"
 	"path/filepath"
+	"strings"
 )
 
 var clientBuildDir, _ = filepath.Abs("client/build")
 
+
 func Run() {
+	dataDir := "."
+	hostPolicy := func(ctx context.Context, host string) error {
+		// Note: change to your real domain
+		allowedHost := "bentekkie.com"
+		if strings.HasPrefix(host,allowedHost) {
+			return nil
+		}
+		return fmt.Errorf("acme/autocert: only %s host is allowed", allowedHost)
+	}
+
+
 	grpcServer := grpc.NewServer()
 	shellServer := mainframe.NewShellServer()
 	pb.RegisterShellServer(grpcServer, shellServer)
@@ -27,4 +44,19 @@ func Run() {
 	log.Println("Listening...")
 	corsObj := handlers.AllowedOrigins([]string{"*"})
 	_ = http.ListenAndServe(":5000", handlers.CORS(corsObj)(rtr))
+	m := &autocert.Manager{
+		Prompt:     autocert.AcceptTOS,
+		HostPolicy: hostPolicy,
+		Cache:      autocert.DirCache(dataDir),
+	}
+	server := &http.Server{
+		Addr:      ":8082",
+		Handler:   rtr,
+		TLSConfig: &tls.Config{
+			GetCertificate: m.GetCertificate,
+		},
+	}
+	go http.ListenAndServe(":5000", m.HTTPHandler(nil))
+
+	log.Fatal(server.ListenAndServeTLS("", ""))
 }
